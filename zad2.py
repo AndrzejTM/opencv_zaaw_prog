9f7b460f512eac1ea994126201640c4c0a321bb9
AndrzejTM <Wampir@12345>
2025-03-08 15:26:02 +0100

    usunięcie plików z pierwszego brancha

--- zad2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-# Wczytanie obrazu z pliku
-
-image = cv2.imread("wiewiorka2.jpg")
-(h, w, c) = image.shape[:3]
-print(f'width: {w} pixels')
-print(f'height: {h} pixels')
-print(f'channels: {c}')
