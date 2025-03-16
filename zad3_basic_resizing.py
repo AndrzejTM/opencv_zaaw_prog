f73fc9d7f5efcdddea27f620c72801cf2e6194ac
AndrzejTM <Wampir@12345>
2025-03-16 17:46:47 +0100

    commit 13

---
+++ zad3_basic_resizing.py
@@ -0,0 +1,17 @@
+import cv2
+# load the original input image and display it on our screen
+image = cv2.imread("wiewiorka2.jpg")
+cv2.imshow("Original", image)
+# perform the resizing
+
+# (h, w) = image.shape[:2]
+# (cX, cY) = (w * 2, h * 2)
+resized = cv2.resize(image, (200, 300), interpolation=cv2.INTER_AREA)
+# cv2.imshow("Resized", resized)
+# Powiększanie obrazu z użyciem metody INTER_LINEAR
+# resized_image = cv2.resize(image, (cX, cY), interpolation=cv2.INTER_LINEAR)
+
+# Zapisz lub wyświetl powiększony obraz
+cv2.imshow("200 x 300", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
