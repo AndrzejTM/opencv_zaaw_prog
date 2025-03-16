493e0eff397f4fe1a09288089fa4b26e34fbfd69
AndrzejTM <Wampir@12345>
2025-03-16 17:16:51 +0100

    commit 8

---
+++ zad3_rotation.py
@@ -0,0 +1,23 @@
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+# load the image and show it
+image = cv2.imread("wiewiorka2.jpg")
+cv2.imshow("Original", image)
+# grab the dimensions of the image and calculate the center of the
+# image
+(h, w) = image.shape[:2]
+#(cX, cY) = (w // 2, h // 2)
+cX = 0
+cY = 0
+# rotate our image by 45 degrees around the center of the image
+M = cv2.getRotationMatrix2D((cX, cY), 30, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+# cv2.imshow("Rotated by 30 Degrees", rotated)
+# rotate our image by -90 degrees around the image
+# M = cv2.getRotationMatrix2D((cX, cY), 30, 1.0)
+# rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("Rotated by 30 Degrees", rotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
